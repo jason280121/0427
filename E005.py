10db2696b2af043077e647087977052d1ecc4bf7
jason280121 <tsou58.tsou@gmail.com>
2025-04-27 13:37:52 +0800

    add all file 20250427 13:37

---
+++ E005.py
@@ -0,0 +1,120 @@
+
+
+#git-scm 上網下載 github  Git-2.49.0-64-bit.exe
+# https://git-scm.com/downloads/win
+# gitHub version Control 版本控制, 如紀錄點
+
+#git  版本控制 ，儲存庫，公開，協作，開放別人共享
+#gitHub 雲端，給git專用，多人協作位置
+#git-lab 另外
+#bit-bucket 另外
+
+
+#本地端
+#修改，新增，刪除，更新，先放置暫存區，上傳到暫存區
+
+
+
+#暫存區
+#暫存區如果覺的沒有問題的話，然後在放置儲存庫
+
+
+#儲存庫
+#放置儲存庫就會有一個紀錄點，可以回到之前的紀錄點，存檔，管控版本
+
+
+# widows + r
+#cmd
+#git -v
+# (git version 2.49.0.windows.1)
+
+
+
+#桌面路徑
+# C:\Users\jason58.tsou\
+#mkdir 0427
+#cd 0427
+
+#建立初始化git 資料夾
+#git init
+
+# 備註說明:
+#.git 版本隱藏檔，不能刪除
+
+# 11:40
+
+#查看設定值
+#git config --list
+
+#完整寫法 二個底線--
+#設定電子郵件
+#git config --global user.email jason58.tsou@gmail.com
+
+#設定名字
+#git config --global user.name jason280121
+
+
+#實際流程 11:59
+#查詢狀態(紅色，本地端)
+#git status
+#Untracked files 未追縱檔案，還在存本地端檔案 ，新增
+
+#檔案加入暫存區(綠色)，單個檔案寫檔名.副檔名
+#git add jason.txt
+
+#資料夾所有紅色加入
+#git add .
+
+#儲存庫  ''空字串 -m(message)
+# git commit -m ""
+# git commit -m 20250427
+# git commit -m "20250427 my first file"
+
+#查看上傳紀錄
+#git log
+
+#重設專案 git init
+#重開pycharm
+
+#Hello
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
